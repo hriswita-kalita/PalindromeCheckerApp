d2d16bc1292c95286487550812257b888ae90e01
Yogesh Yadav <yogeshyadavv99@gmail.com>
2026-03-11 22:53:42 +0530

    UC1 - Application Entry & Welcome Message

---
+++ src/UC1.java
@@ -0,0 +1,17 @@
+public class UC1 {
+
+    public static void main(String[] args) {
+
+        // Application Name
+        System.out.println("PalindromeChecker Application");
+
+        // Application Version
+        System.out.println("Version: 1.0");
+
+        // Welcome Message
+        System.out.println("Welcome to the Palindrome Checker App!");
+
+        // Application Flow Message
+        System.out.println("Program is ready to continue to the next use case...");
+    }
+}
